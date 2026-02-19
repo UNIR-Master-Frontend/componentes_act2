d6f7c5849a09edff4435e0831e817b18df7d08c8
DanielaLozano04 <dl@iqthink.com>
2026-02-19 10:32:58 -0500

    corregir test de form

--- src/features/coworking/components/ReservationForm/ReservationForm.test.tsx
+++ src/features/coworking/components/ReservationForm/ReservationForm.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { ReservationForm } from "./ReservationForm";
 
 describe("ReservationForm (mínimo)", () => {
