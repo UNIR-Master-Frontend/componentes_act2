b7bd076ff96cc96e26118c85ec64c473886a5305
dahcami <dahcami@gmail.com>
2026-04-11 01:38:22 -0500

    Se actualizan imagenes libros y revistas

--- src/features/library/components/Book/Book.tsx
+++ src/features/library/components/Book/Book.tsx
@@ -2,11 +2,25 @@ import type { Book } from "../../interfaces/book.interface";
 import Card from "../../../../components/Card/Card";
 
 import bookImg from "../../../../assets/images/jpg/book.jpg";
+import img1 from "../../../../assets/images/png/book1.png";
+import img2 from "../../../../assets/images/png/book2.png";
+import img3 from "../../../../assets/images/png/book3.png";
+import img4 from "../../../../assets/images/png/book4.png";
+import img5 from "../../../../assets/images/png/book5.png";
+import img6 from "../../../../assets/images/png/book6.png";
+import img7 from "../../../../assets/images/png/book7.png";
+import img8 from "../../../../assets/images/png/book8.png";
+import img9 from "../../../../assets/images/png/book9.png";
+import img10 from "../../../../assets/images/png/book10.png";
 import "./styles.css";
 import { useNavigate } from "react-router";
 
+const imagesbooks = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
+
 export default function Book({ book }: { book: Book }) {
   const navigate = useNavigate();
+
+  const imagesbooksrandom = imagesbooks[Math.floor(Math.random() * imagesbooks.length)];
 
   const goToDetail = () => {
     navigate(`/library/books/${book.id}`);
@@ -15,7 +29,7 @@ export default function Book({ book }: { book: Book }) {
   return (
     <Card onClick={goToDetail}>
       <div className="book-img">
-        <img src={bookImg} alt="Imagen de libro" />
+        <img src={imagesbooksrandom} alt="Imagen de libro" />
       </div>
       <div className="book-content">
         <h4>{book.nombre}</h4>
--- src/features/library/components/Magazine/Magazine.tsx
+++ src/features/library/components/Magazine/Magazine.tsx
@@ -2,10 +2,24 @@ import { useNavigate } from "react-router";
 import "./styles.css";
 import type { Magazine } from "../../interfaces/magazine.interface";
 import magazineImg from "../../../../assets/images/jpg/magazine.jpg";
+import img1 from "../../../../assets/images/png/mag1.png";
+import img2 from "../../../../assets/images/png/mag2.png";
+import img3 from "../../../../assets/images/png/mag3.png";
+import img4 from "../../../../assets/images/png/mag4.png";
+import img5 from "../../../../assets/images/png/mag5.png";
+import img6 from "../../../../assets/images/png/mag6.png";
+import img7 from "../../../../assets/images/png/mag7.png";
+import img8 from "../../../../assets/images/png/mag8.png";
+import img9 from "../../../../assets/images/png/mag9.png";
+import img10 from "../../../../assets/images/png/mag10.png";
 import Card from "../../../../components/Card/Card";
+
+const imagesmagazines = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
 
 export default function Magazine({ magazine }: { magazine: Magazine }) {
   const navigate = useNavigate();
+
+   const imagesmagazinesrandom = imagesmagazines[Math.floor(Math.random() * imagesmagazines.length)];
 
   const goToDetail = () => {
     navigate(`/library/magazines/${magazine.id}`);
@@ -14,7 +28,7 @@ export default function Magazine({ magazine }: { magazine: Magazine }) {
   return (
     <Card onClick={goToDetail}>
       <div className="book-img">
-        <img src={magazineImg} alt="Imagen de revista" />
+        <img src={imagesmagazinesrandom} alt="Imagen de revista" />
       </div>
       <div className="magazine-content">
         <h4>Revista edición {magazine.edicion}</h4>
